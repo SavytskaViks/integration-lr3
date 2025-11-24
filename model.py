11d765735cc081bbb87a26466e57eb4193c7fe6d
SavytskaViks <viktoriia.savytska8@gmail.com>
2025-11-25 00:50:21 +0200

    fix model flatten dimension

--- model.py
+++ model.py
@@ -2,19 +2,26 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class SpeechCommandModel(nn.Module):
     def __init__(self, num_classes=4):
         super().__init__()
 
         self.conv1 = nn.Conv2d(1, 8, kernel_size=3)
         self.conv2 = nn.Conv2d(8, 16, kernel_size=3)
-        self.fc1 = nn.Linear(16 * 61 * 61, 64)
+
+        # Автоматичний розрахунок розміру для fc1
+        with torch.no_grad():
+            dummy = torch.zeros(1, 1, 64, 64)
+            out = self.conv2(F.relu(self.conv1(dummy)))
+            flat_dim = out.numel()
+
+        self.fc1 = nn.Linear(flat_dim, 64)
         self.fc2 = nn.Linear(64, num_classes)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))     # (B, 8, H, W)
-        x = F.relu(self.conv2(x))     # (B,16,H,W)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         return self.fc2(x)
+
